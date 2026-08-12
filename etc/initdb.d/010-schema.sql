221825ed7da70fd0cf52ae57a433f5cc9bd95d11
Or Hayat <or.hayat@volumez.com>
2026-08-12 08:23:33 +0300

    fix(storage): correct producer join in txn offset debug views
    v_txn_consumer_offset_tp and v_txn_produce_offset joined producer_epoch
    on pe.producer = pe.id, matching rows by coincidence of id sequences
    instead of by producer.

--- etc/initdb.d/010-schema.sql
+++ etc/initdb.d/010-schema.sql
@@ -439,7 +439,7 @@ from
     cluster c
     join topic t on t.cluster = c.id
     join producer p on p.cluster = c.id
-    join producer_epoch pe on pe.producer = pe.id
+    join producer_epoch pe on pe.producer = p.id
     join txn on txn.cluster = c.id
     and txn.producer = p.id
     join txn_detail txn_d on txn_d.transaction = txn.id
@@ -496,7 +496,7 @@ from
     cluster c
     join consumer_group cg on cg.cluster = c.id
     join producer p on p.cluster = c.id
-    join producer_epoch pe on pe.producer = pe.id
+    join producer_epoch pe on pe.producer = p.id
     join txn on txn.cluster = c.id
     and txn.producer = p.id
     join txn_detail txn_d on txn_d.transaction = txn.id
